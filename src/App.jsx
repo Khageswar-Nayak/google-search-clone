d463f5e005dbc272cf67f354f2249dad9663bde2
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-12-27 00:01:44 +0530

    implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import SearchResult from "./components/SearchResult";
+// import SearchResult from "./components/SearchResult";
 import { AppContext } from "./utils/ContextApi";
+import { lazy, Suspense } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const SearchResult = lazy(() => import("./components/SearchResult"));
 
 function App() {
-    return (
-        <AppContext>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" exact element={<Home />} />
-                    <Route
-                        path="/:query/:startIndex"
-                        exact
-                        element={<SearchResult />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </AppContext>
-    );
+  return (
+    <AppContext>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route
+            path="/:query/:startIndex"
+            exact
+            element={
+              <Suspense
+                fallback={
+                  <div class="flex justify-center items-center h-screen">
+                    <CircularProgress />
+                  </div>
+                }
+              >
+                <SearchResult />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AppContext>
+  );
 }
 
 export default App;
